2ce11b9688eb2deaa988df8cf880e52a7758c0b3
Yifan Shi <stevenls1118@msn.com>
2022-10-23 19:52:07 -0600

    modified to make it work

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
+  }
+
+  let verticalarray = [];
+
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalarray.push([]);
+  }
+
+  for (i of letters) {
+    for (let j =0; j < i.length; j++) {
+      verticalarray[j].push(i[j])
+    }
+  }
+
+  const verticalJoin = verticalarray.map(ls => ls.join(''));
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;    
 }
 
 module.exports = wordSearch
+
+// CODED BY: Clayton Van Imschoot and Steven Shi
